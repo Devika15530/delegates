90b0c410013dca3843bac8bca183d10874195e40
Devika15530 <60097323+Devika15530@users.noreply.github.com>
2022-02-08 10:50:27 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    //delegate is a type safe function pointer.It holds the refernce of a method and when we
+    //call delegate it interally call the method
+
+    public delegate void mydelegate(string s);//Declaring the delegate return type and parameter type should be same as method that is calling 
+    class Program
+    {
+
+        public void SendMessage(string s)
+        {
+            Console.WriteLine(s + "from  delegate");
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            mydelegate de = new mydelegate(p.SendMessage);//binding the method with delegate
+            de("heyy");//calling delegate
+            Console.Read();
+        }
+    }
+
+}
---
+++ Usageofdelegates.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    public class Usageofdelegates
+    {
+        static void main()
+        {
+            List<Employee> emplist = new List<Employee>();
+            emplist.Add(new Employee() { empid = 101, empname = "Devika", experience = 12, salary = 120000 });
+            emplist.Add(new Employee() { empid = 102, empname = "palak", experience = 15, salary = 120000 });
+            emplist.Add(new Employee() { empid = 103, empname = "sai", experience = 3, salary = 120000 });
+            emplist.Add(new Employee() { empid = 104, empname = "ab", experience = 4, salary = 120000 });
+
+            IsPromotable ispt = new IsPromotable(Promote);
+            Employee.promoteEmployee(emplist, ispt);
+            Console.Read();
+        }
+        public static bool Promote(Employee emp)
+        {
+            if(emp.experience>10)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+   public delegate bool IsPromotable(Employee e);
+   public class Employee
+    {
+        public int empid { get; set; }
+        public string empname { get; set; }
+        public int salary { get; set; }
+        public int experience { get; set; }
+
+        public static void promoteEmployee(List<Employee> emplist,IsPromotable isp)
+        {
+            foreach(Employee emp in emplist)
+            {
+                if (isp(emp))
+                {
+                    Console.WriteLine(emp.empname + "Promoted");
+                }
+
+            }
+
+        }
+    }
+}
